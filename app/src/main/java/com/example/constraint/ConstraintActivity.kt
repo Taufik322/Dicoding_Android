cf50b51984b1e4f2bde33a0bb3a3a98f1e1886a1
Muhammad Taufik Hidayat <79823539+Taufik322@users.noreply.github.com>
2022-02-27 16:58:17 +0800

    add latihan constraint

---
+++ app/src/main/java/com/example/constraint/ConstraintActivity.kt
@@ -0,0 +1,12 @@
+package com.example.constraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dicodingandroid.R
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
