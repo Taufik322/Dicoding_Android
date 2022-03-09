75a467df9031ec0c9c4f6130d843b1af1269aae3
Muhammad Taufik Hidayat <79823539+Taufik322@users.noreply.github.com>
2022-03-09 11:39:24 +0800

    add navigation drawer

--- app/src/main/java/com/example/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/fragment/HomeFragment.kt
@@ -15,7 +15,7 @@ class HomeFragment : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home_frag, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
