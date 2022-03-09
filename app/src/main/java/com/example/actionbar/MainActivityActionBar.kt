9e6b4b92cad3d6fecaa31bfc8e8d165d80f5bdc0
Muhammad Taufik Hidayat <79823539+Taufik322@users.noreply.github.com>
2022-03-09 11:04:12 +0800

    add actionbar search

--- app/src/main/java/com/example/actionbar/MainActivityActionBar.kt
+++ app/src/main/java/com/example/actionbar/MainActivityActionBar.kt
@@ -1,10 +1,14 @@
 package com.example.actionbar
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import android.widget.Toast
 import com.example.dicodingandroid.R
 import com.example.dicodingandroid.databinding.ActivityMainActionBarBinding
 
@@ -21,6 +25,29 @@ class MainActivityActionBar : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu!!.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /*
+            Gunakan method ini ketika search selesai atau OK
+             */
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivityActionBar, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            /*
+            Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+             */
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
         return true
     }
 
